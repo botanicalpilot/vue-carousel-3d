94ad5a1813f37280815456b0abe098f4e423c79a
Stephen Ouldhouse <stephenouldhouse@Stephens-MacBook-Pro.local>
2023-12-21 13:27:52 -0800

    renamed slide to dSlide and rebuilt project

--- src/carousel-3d/index.js
+++ src/carousel-3d/index.js
@@ -3,7 +3,7 @@ import Carousel3d from "@/carousel-3d/Carousel3d";
 
 const install = (Vue) => {
     Vue.component('carousel3d', Carousel3d)
-    Vue.component('slide', Slide)
+    Vue.component('dSlide', Slide)
 }
 
 export default {
